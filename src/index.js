351c0779639da83c238cbde854a3adbe6831cf2f
stefka11 <sph_bg@abv.bg>
2025-01-17 21:16:47 +0200

    add and setup handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',   //задаваме да моге да обработва с разширение hbs
+}));
+app.set('view engine', 'hbs');    //hbs ще е дефолтния енжин
+app.set('views', './src/views');  //указваме къде се намира папката views
+
 app.get ('/', (req, res) => {
-    res.send('It works!');
+    //res.send('It works!');
+    res.render('home', {layout: false});
 });
 
 app.listen(5000, ()=> console.log('Server is listening at http://localhost:5000...'));
